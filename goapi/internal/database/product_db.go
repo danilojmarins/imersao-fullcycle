3f37139f1cd1b599547f199fefcde6f27a501114
danilojmarins <danilojmarins@hotmail.com>
2024-01-30 20:16:59 -0300

    Database and Service Layers.

--- goapi/internal/database/product_db.go
+++ goapi/internal/database/product_db.go
@@ -14,8 +14,8 @@ func NewProductDB(db *sql.DB) *ProductDB {
 	return &ProductDB{db: db}
 }
 
-func (cd *ProductDB) GetProducts() ([]*entity.Product, error) {
-	rows, err := cd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products")
+func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
+	rows, err := pd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products")
 	if err != nil {
 		return nil, err
 	}
@@ -33,17 +33,49 @@ func (cd *ProductDB) GetProducts() ([]*entity.Product, error) {
 	return products, nil
 }
 
-func (cd *ProductDB) GetProductById(id string) (*entity.Product, error) {
+func (pd *ProductDB) GetProductById(id string) (*entity.Product, error) {
 	var product entity.Product
-	err := cd.db.QueryRow("SELECT id, name, description, price, category_id, image_url FROM products WHERE id = ?", id).Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL)
+	err := pd.db.QueryRow(
+		"SELECT id, name, description, price, category_id, image_url FROM products WHERE id = ?",
+		id,
+	).Scan(
+		&product.ID,
+		&product.Name,
+		&product.Description,
+		&product.Price,
+		&product.CategoryID,
+		&product.ImageURL,
+	)
 	if err != nil {
 		return nil, err
 	}
 	return &product, nil
 }
 
-func (cd *ProductDB) CreateProduct(product *entity.Product) (string, error) {
-	_, err := cd.db.Exec("INSERT INTO products (id, name, description, price, category_id, image_url) VALUES (?, ?, ?, ?, ?, ?)", product.ID, product.Name, product.Description, product.Price, product.CategoryID, product.ImageURL)
+func (pd *ProductDB) GetProductByCategoryId(categoryId string) ([]*entity.Product, error) {
+	rows, err := pd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products WHERE category_id = ?", categoryId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*entity.Product
+	for rows.Next() {
+		var product entity.Product
+		if err := rows.Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL); err != nil {
+			return nil, err
+		}
+		products = append(products, &product)
+	}
+
+	return products, nil
+}
+
+func (pd *ProductDB) CreateProduct(product *entity.Product) (string, error) {
+	_, err := pd.db.Exec(
+		"INSERT INTO products (id, name, description, price, category_id, image_url) VALUES (?, ?, ?, ?, ?, ?)",
+		product.ID, product.Name, product.Description, product.Price, product.CategoryID, product.ImageURL,
+	)
 	if err != nil {
 		return "", err
 	}
--- goapi/internal/entity/product.go
+++ goapi/internal/entity/product.go
@@ -2,18 +2,6 @@ package entity
 
 import "github.com/google/uuid"
 
-type Category struct {
-	ID   string
-	Name string
-}
-
-func NewCategory(name string) *Category {
-	return &Category{
-		ID:   uuid.New().String(),
-		Name: name,
-	}
-}
-
 type Product struct {
 	ID          string
 	Name        string
