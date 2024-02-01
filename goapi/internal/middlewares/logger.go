dc2a8ca01f470ac8ab53e21469d96da2af1bf2d6
danilojmarins <danilojmarins@hotmail.com>
2024-02-01 00:36:52 -0300

    Logger Middleware.

--- goapi/internal/middlewares/logger.go
+++ goapi/internal/middlewares/logger.go
@@ -1,7 +1,32 @@
 package middlewares
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type ResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewResponseWriter(w http.ResponseWriter) *ResponseWriter {
+	return &ResponseWriter{w, http.StatusOK}
+}
+
+func (rw *ResponseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
 
 func Logger(next http.Handler) http.Handler {
-	return next
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			rw := NewResponseWriter(w)
+			start := time.Now()
+			next.ServeHTTP(rw, r)
+			fmt.Println("status:", rw.statusCode, "duration:", time.Since(start), "method:", r.Method, "path:", r.URL.EscapedPath())
+		},
+	)
 }
