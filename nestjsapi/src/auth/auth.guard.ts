2013c2fd4297576cc1b3437d6614f0234b045527
danilojmarins <danilojmarins@hotmail.com>
2024-02-14 23:57:01 -0300

    Auth Guard

--- nestjsapi/src/auth/auth.guard.ts
+++ nestjsapi/src/auth/auth.guard.ts
@@ -1,11 +1,41 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const request: Request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new HttpException(
+        'Token de autenticação obrigatório',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    try {
+      const payload = this.jwtService.verify(token);
+      request.user = payload;
+      return true;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.UNAUTHORIZED);
+    }
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
--- nestjsapi/src/auth/auth.module.ts
+++ nestjsapi/src/auth/auth.module.ts
@@ -1,19 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { AuthGuard } from './auth.guard';
 
+@Global()
 @Module({
   imports: [
     JwtModule.register({
       secret: 'd8d1a50e-698f-4f35-8e95-dfafc7b197ef',
       signOptions: { expiresIn: '1d' },
+      global: true,
     }),
     TypeOrmModule.forFeature([User]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
+  exports: [AuthGuard],
 })
 export class AuthModule {}
